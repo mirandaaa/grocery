13ba1d1ce5b7c56e291bf33263fc74a34e45beca
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-08-04 17:37:33 -0400

    Changing model names to be one word.

--- db/migrate/20160804205239_create_food_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateFoodItems < ActiveRecord::Migration[5.0]
-  def change
-    create_table :food_items do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20160804205547_create_food_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateFoodCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :food_categories do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
