e094ef2d8c4d030b32847a11283124b9b00e9f76
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-08-08 16:36:11 -0400

    Pending functions for defining item list on a Grocery List.

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -2,7 +2,12 @@ class GroceryList < ApplicationRecord
   has_many :grocery_items
   has_many :items, through: :grocery_items
 
-  def item_list
-    items.collect(&:name).join(', ')
-  end
+  # def item_list
+  #   items.collect(&:name).join(', ')
+  # end
+
+  # def item_list=(item_name)
+  #   new_or_found_item = Item.find_or_create_by(name: item_name)
+  #   self.items += new_or_found_item
+  # end
 end
