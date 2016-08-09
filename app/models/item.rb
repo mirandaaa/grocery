23acc1422eeeb590d5da53a6f4a5e5aa9bc4f06c
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-08-09 16:30:34 -0400

    Fixing DB tables.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :category
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :case_sensitive => false
 
-  before_validation :downcase_name
-
-  def downcase_name
-    self.name.downcase!
-  end
+  # before_save :downcase_name
+  #
+  # def downcase_name
+  #   self.name.downcase!
+  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809202116) do
+ActiveRecord::Schema.define(version: 20160809202919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.integer  "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_categories_on_item_id", using: :btree
   end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +46,5 @@ ActiveRecord::Schema.define(version: 20160809202116) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "items", "categories"
 end
