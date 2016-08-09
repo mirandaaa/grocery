ed6725f4c6b0ca62c64edfe26b7cbbb3458823d1
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-08-09 12:02:34 -0400

    Implementing edit and delete functionality for lists.

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -16,10 +16,32 @@ class GroceryListsController < ApplicationController
   def create
     @list = GroceryList.new(list_params)
     @list.save
+
+    flash.notice = "Grocery List '#{@list.name}' Created!"
+
     redirect_to grocery_list_path(@list)
   end
 
+  def edit
+    @list = GroceryList.find(params[:id])
+  end
+
   def update
+    @list = GroceryList.find(params[:id])
+    @list.update(list_params)
+
+    flash.notice = "Grocery List '#{@list.name}' Updated!"
+
+    redirect_to grocery_list_path(@list)
+  end
+
+  def destroy
+    @list = GroceryList.find(params[:id])
+    @list.destroy
+
+    flash.notice = "Grocery List '#{@list.name}' Deleted!"
+
+    redirect_to grocery_lists_path
   end
 
 end
