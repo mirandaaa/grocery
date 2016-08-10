c5bf03889b8433e6be55e2bc9fe811de90fb50de
Miranda Wang <msw3bb@virginia.edu>
2016-08-10 17:35:11 -0400

    Allowing delete on list by making items dependent on destroy.

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -1,5 +1,5 @@
 class GroceryList < ApplicationRecord
-  has_many :grocery_items
+  has_many :grocery_items, dependent: :destroy 
   has_many :items, :through => :grocery_items
 
   validates_presence_of :name, :desc
