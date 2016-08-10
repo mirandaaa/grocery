d1740b1979471ecd92e8f6c00a71f6f49a786e2a
Miranda Wang <msw3bb@virginia.edu>
2016-08-10 16:51:02 -0400

    Fixing add_items and categories methods on list model

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -6,14 +6,14 @@ class GroceryList < ApplicationRecord
 
   scope :recent, ->{ order("created_at desc").limit(3) }
 
-  def add_item(item_name)
-    new_or_found_item = Item.find_or_create_by(name: item_name)
-    # Get category too...
+  def add_item(item_name, category_name)
+    category = Category.where(name: category_name).first
+    new_or_found_item = Item.find_or_create_by(name: item_name, category: category)
     self.items += new_or_found_item
   end
 
   def categories
-    self.items.map { |item| item.category.name }.uniq
+    self.items.map { |item| item.category }.uniq
   end
 
 end
