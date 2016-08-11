28dab744895e0aec8ab14c12929dd4c6b4784cf2
Miranda Wang <msw3bb@virginia.edu>
2016-08-11 14:59:20 -0400

    Refactoring controller and deleting unused code.

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,5 +1,10 @@
 class GroceryListsController < ApplicationController
   include GroceryListsHelper
+  before_action :find_list, only: [:show, :edit, :update, :destroy, :add_item, :delete_item]
+
+  def find_list
+    @list = GroceryList.find(params[:id])
+  end
 
   def index
     @lists = GroceryList.all
@@ -10,52 +15,39 @@ class GroceryListsController < ApplicationController
   end
 
   def show
-    @list = GroceryList.find(params[:id])
   end
 
   def create
     @list = GroceryList.new(list_params)
     @list.save
-
     flash.notice = "Grocery List '#{@list.name}' Created!"
-
     redirect_to grocery_list_path(@list)
   end
 
   def edit
-    @list = GroceryList.find(params[:id])
   end
 
   def update
-    @list = GroceryList.find(params[:id])
     @list.update(list_params)
-
     flash.notice = "Grocery List '#{@list.name}' Updated!"
-
     redirect_to grocery_list_path(@list)
   end
 
   def destroy
-    @list = GroceryList.find(params[:id])
     @list.destroy
-
     flash.notice = "Grocery List '#{@list.name}' Deleted!"
-
     redirect_to grocery_lists_path
   end
 
   def add_item
-    @list = GroceryList.find(params[:id])
     @category = Category.find(params[:grocery_list][:category_id])
     @list.add_item(params[:grocery_list][:item_name], @category.name)
-    redirect_to grocery_list_path(@list.id)
+    redirect_to grocery_list_path(@list)
   end
 
   def delete_item
-    @list = GroceryList.find(params[:id])
     @item = Item.find(params[:item_id])
     @list.items.delete(@item)
-    redirect_to grocery_list_path(@list.id)
+    redirect_to grocery_list_path(@list)
   end
-
 end
--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -11,9 +11,4 @@ class GroceryList < ApplicationRecord
     new_or_found_item = Item.find_or_create_by(name: item_name, category: category)
     grocery_item = GroceryItem.create(item: new_or_found_item, grocery_list: self)
   end
-
-  def categories
-    self.items.map { |item| item.category }.uniq
-  end
-
 end
